c0f6395dc77a4939578d674db6e6613ab63659d8
suxrob-developer <30suxrob01komolov1999@gmail.com>
2024-03-03 07:35:15 +0500

    Nav and header

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Toggle Navbar function
+function Toggle(){
+    const NavItems = document.querySelector(".nav-items")
+    const Bars = document.querySelector(".bars")
+    NavItems.classList.toggle('toggle')
+    Bars.classList.toggle('active')
+}
